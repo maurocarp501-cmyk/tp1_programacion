0c9a1eafd463974e350e2b0a3056be8ec495c74d
mauro <maurocarp501@gmail.com>
2026-05-26 20:06:09 -0300

    nueva modalidad en diseño de tarjetas

--- src/componentes2/Tarjeta.jsx
+++ src/componentes2/Tarjeta.jsx
@@ -1,20 +1,18 @@
 export default function Tarjeta({nombre,categoria,prioridad,estado,eliminar,cambiarEstado}) {
-
     return(
-  <div className="Tarjeta">
-<div className="tarjeta-info">
-    <h2>{nombre}</h2>
-            <h3>{categoria}, {prioridad},{estado}</h3>
-             </div>    
-    <span
-    className="Eliminar"
-               onClick={() => eliminar()}
-    >Borrar</span>   
-    <select value={estado} onChange={(e) => cambiarEstado(e.target.value)}>
-    <option value="pendiente">pendiente</option>
-    <option value="en proceso">en proceso</option>
-    <option value="finalizado">finalizado</option>
-    </select>
-  </div>
+        <div className="Tarjeta">
+            <div className="tarjeta-info">
+                <h2>{nombre}</h2>
+                <h3>{categoria}, {prioridad}</h3>
+            </div>
+            <div className="tarjeta-footer">
+                <select value={estado} onChange={(e) => cambiarEstado(e.target.value)}>
+                    <option value="pendiente">pendiente</option>
+                    <option value="en proceso">en proceso</option>
+                    <option value="finalizado">finalizado</option>
+                </select>
+                <span className="Eliminar" onClick={() => eliminar()}>Borrar</span>
+            </div>
+        </div>
     )
 }
